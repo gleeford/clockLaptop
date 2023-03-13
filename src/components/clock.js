bda3af9563f2fbd9d89e16a05b0febacc7f3a3b1
gleeford <mgmromasanta@gmail.com>
2023-03-13 18:49:26 +0800

    Act 3 - added time Diffence 	modified:   src/App.css 	modified:   src/components/clock.js 	modified:   src/index.js

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,38 +1,73 @@
-import React from 'react';
-import ReacDOM from 'react-dom';
+import React from "react";
+import ReacDOM from "react-dom";
 
-class Clock extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {time : new Date()};
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date(),
+      timeIn: null,
+      timeOut: null,
+      timeDiff: null,
     };
+  }
 
+  componentDidMount() {
+    this.timer = setInterval(() => this.tick(), 1000);
+  }
 
-componentDidMount(){
-    this.timer = setInterval(
-        () => this.tick(), 
-        1000
-    );
-}
-
-componentWillUnmount(){
+  componentWillUnmount() {
     clearInterval(this.timer);
-}
+  }
 
-tick(){
+  tick() {
     this.setState({
-        time : new Date(),
+      time: new Date(),
     });
+  }
+
+  clockIn = () => {
+    this.setState({
+      timeIn: this.state.time,
+    });
+  };
+
+  clockOut = () => {
+    this.setState({
+      timeOut: this.state.time,
+    });
+  };
+
+  clockDiff = () => {
+    const { timeIn, timeOut } = this.state;
+    if (!timeIn || !timeOut) {
+      return;
+    }
+    const diff = Math.abs(timeOut - timeIn);
+    const hours = Math.floor(diff / (1000 * 60 * 60));
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+    const timeDiff = `${hours}h ${minutes}m ${seconds}s`;
+    this.setState({
+      timeDiff: timeDiff,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App-header">
+        <h1>CLOCK</h1>
+        <h2>{this.state.time.toLocaleTimeString()} </h2>
+        <h4>Time In: {this.state.timeIn && this.state.timeIn.toLocaleTimeString()}</h4>
+        <button className="btn" onClick={this.clockIn}>Time In</button> 
+        <h4>Time Out: {this.state.timeOut && this.state.timeOut.toLocaleTimeString()}</h4>
+        <button className="btn" onClick={this.clockOut}>Time Out</button>
+        <h4>Time Difference: {this.state.timeDiff}</h4>
+        <button className="btn" onClick={this.clockDiff}>Time Difference</button>
+      </div>
+    );
+  }
 }
 
-render(){
-     return(
-        <div>
-            <h1>CLOCK</h1>
-            <h3>{this.state.time.toLocaleTimeString()} </h3>
-        </div>
-     );
-}
-}
 
 export default Clock;
